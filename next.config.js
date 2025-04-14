4194c0ce42c42b0718f1406320ead30cc4def025
anas-derk <anas.derk2023@gmail.com>
2025-04-15 01:57:23 +0300

    Continue

--- next.config.js
+++ next.config.js
@@ -6,34 +6,34 @@ const nextConfig = {
         removeConsole: process.env.NODE_ENV === "production",
     },
     env: {
-        BASE_API_URL: process.env.NODE_ENV === "development" ? "http://localhost:5200" : "https://api.ubuyblues.com",
-        WEBSITE_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ubuyblues.com",
-        USER_TOKEN_NAME_IN_LOCAL_STORAGE: "ub-s-u-t",
-        STORE_NAME: "Ubuyblues Store",
-        USER_LANGUAGE_FIELD_NAME_IN_LOCAL_STORAGE: "ubuyblues-store-language",
-        USER_ADDRESSES_FIELD_NAME_IN_LOCAL_STORAGE: "ubuyblues-customer-addresses",
-        USER_CART_NAME_IN_LOCAL_STORAGE: "ubuyblues-store-customer-cart",
-        USER_THEME_MODE_FIELD_NAME_IN_LOCAL_STORAGE: "ubuyblues-store-light-mode",
-        REFERAL_WRITER_FIELD_NAME_IN_LOCAL_STORAGE: "ubuyblue-store-referal-writer-info",
-        SELECTED_COUNTRY_BY_USER: "ubuyblues-store-country",
-        CONTACT_NUMBER: "96560048235",
-        CONTACT_EMAIL: "info@ubuyblues.com",
-        FACEBOOK_LINK: "https://www.facebook.com/ubuyblues?mibextid=ZbWKwL",
-        INSTAGRAM_LINK: "https://www.instagram.com/ubuyblues",
+        BASE_API_URL: process.env.NODE_ENV === "development" ? "http://localhost:5200" : "https://api.ciratco.com",
+        WEBSITE_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ciratco.com",
+        USER_TOKEN_NAME_IN_LOCAL_STORAGE: "c-s-u-t",
+        STORE_NAME: "Ciratco Store",
+        USER_LANGUAGE_FIELD_NAME_IN_LOCAL_STORAGE: "ciratco-store-language",
+        USER_ADDRESSES_FIELD_NAME_IN_LOCAL_STORAGE: "ciratco-customer-addresses",
+        USER_CART_NAME_IN_LOCAL_STORAGE: "ciratco-store-customer-cart",
+        USER_THEME_MODE_FIELD_NAME_IN_LOCAL_STORAGE: "ciratco-store-light-mode",
+        REFERAL_WRITER_FIELD_NAME_IN_LOCAL_STORAGE: "ciratco-store-referal-writer-info",
+        SELECTED_COUNTRY_BY_USER: "ciratco-store-country",
+        CONTACT_NUMBER: "4915563191873",
+        CONTACT_EMAIL: "info@ciratco.com",
+        FACEBOOK_LINK: "https://www.facebook.com/ciratco?mibextid=kFxxJD",
+        INSTAGRAM_LINK: "https://www.instagram.com/ciratco",
         X_LINK: "https://x.com/ubuyblues",
-        TIKTOK_LINK: "https://www.tiktok.com/@ubuyblues",
-        WEBSITE_NAME: "Ubuyblues",
-        WEBSITE_DASHBOARD_URL: process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://dashboard.ubuyblues.com"
+        TIKTOK_LINK: "https://www.tiktok.com/@cirat.co?_t=8oZFsy0KQ2z&_r=1",
+        WEBSITE_NAME: "Ciratco",
+        WEBSITE_DASHBOARD_URL: process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://dashboard.ciratco.com"
     },
     async headers() {
         return [
             {
-                source: process.env.NODE_ENV === "development" ? "//localhost:5200/(.*)" : "//api.ubuyblues.com/(.*)",
+                source: process.env.NODE_ENV === "development" ? "//localhost:5200/(.*)" : "//api.ciratco.com/(.*)",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     {
                         key: "Access-Control-Allow-Origin",
-                        value: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ubuyblues.com",
+                        value: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ciratco.com",
                     },
                     {
                         key: "Access-Control-Allow-Methods",
