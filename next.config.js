3d8ea5a2e8482ad941ef80ae6d65c5708ea064f1
anas-derk <anas.derk2023@gmail.com>
2025-04-15 23:42:17 +0300

    Fix

--- next.config.js
+++ next.config.js
@@ -23,7 +23,8 @@ const nextConfig = {
         // X_LINK: "https://x.com/ubuyblues",
         TIKTOK_LINK: "https://www.tiktok.com/@cirat.co?_t=8oZFsy0KQ2z&_r=1",
         WEBSITE_NAME: "Ciratco",
-        WEBSITE_DASHBOARD_URL: process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://dashboard.ciratco.com"
+        WEBSITE_DASHBOARD_URL: process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://dashboard.ciratco.com",
+        MAIN_COLOR_ONE: "#1b405a"
     },
     async headers() {
         return [
